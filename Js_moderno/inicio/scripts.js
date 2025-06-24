bd6ae4c5199a01d2e83a2470b8b943bbbd28ebbc
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-06-24 15:50:08 -0300

    Js avançado

---
+++ Js_moderno/inicio/scripts.js
@@ -0,0 +1,95 @@
+/*
+    ECMAScript -> Associação Europeia dos Fabricantes de Computadores
+    é uma associação que realiza a especificação e padronização de sistemas
+    da informação. ECMA + JavaScript, resumindo, o JavaScript é a linguagem
+    que vc escreve e executa nos navegadores e o ECMAScript é a especificação
+    que define como a linguagem deve se comportar. Existem versões EX:ES6,ES&
+
+    Sempre que uma nova atualização acontece no Js ela é lançada pelo ECMAScript
+    que pode ser abreviada como ES
+
+
+ Strict Mode -> modo estrito --> ativando esse modo, os erros
+ que eram silenciosos passam a gerar exceções no JavaScript
+
+"use strict" // escopo global aplica pra tudo
+
+function showMessage(){
+
+    let personName = "Rodrigo Gonçalves"
+    console.log("Olá", personName)
+}
+
+showMessage()
+
+class Student{
+    get point(){
+        return 7
+    }
+}
+
+let student = new Student()
+console.log(student.point)
+// student.point = 10 --> retorna erro
+
+// delete window.document -> tentando deletar uma propriedade que nn pode ser deletada
+
+function sum(a, a, c){
+   return a + a + c  //reclama que tem um parametro duplicado
+}
+
+const result = sum(1, 3, 2)
+console.log(result) // --> sem modo restrito dá 8
+// pq ele sobrepõe o 1 parametro entao 3 + 3 + 2
+
+Destructuring assignment (desestruturação) permite extrair dados
+de arrays ou objetos em variáveis distintas
+
+
+
+const data = ["Ronaldo", "Ronaldo@gmail.com"]
+
+// Desestruturando
+const [username, email] = data
+console.log("Nome:",username)
+console.log("Email:",email)
+
+const fruits = ["banana", "maça", "laranja"]
+// desestruturar somente o primeiro
+const [item] = fruits
+console.log(item)
+
+// ignorando o 1 na desestruturação usar _
+const[_, apple] = fruits
+console.log(apple)
+
+
+
+// Desestruturar objetos
+
+const product = {
+    description: "Teclado",
+    price: 150,
+}
+
+const { description, price} = product
+console.log("Descrição:", description)
+console.log("Preço: R$", price)
+
+function newProduct(description, price){
+    console.log("### NOVO PRODUTO ###")
+    console.log("Descrição:", description)
+    console.log("Preço: R$", price)
+}
+
+//newProduct("Mouse", 70)// tem q ser na ordem
+// melhor jeito
+
+newProduct({
+    description: "mouse",
+    price: 70
+})
+    
+Rest params (...) permite representar um número indefinido
+de argumentos como um array
+*/
