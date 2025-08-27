05f9869e59912d9d2e068c479d92cdb7992978a4
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-08-27 15:52:52 -0300

    Conhecendo os compiladores no Js

---
+++ Js_moderno/inicio/compiladores/scripts.js
@@ -0,0 +1,14 @@
+/*
+    Compiladores Js -> Converte código escrito em versão
+específica do javascript para outra versão, permitindo usar
+recursos mais recentes da linguagem mesmo em ambientes que
+não oferecem suporte a essas funcionalidades (Ex: navegado-
+res antigos)
+
+    Transpilação -> é um termo usado para se referir a comp
+    ilação para versões anteriores.
+
+    Parser -> Transformer -> Generator
+    mapeia todos os elementos do codigos Js -> Manipula todos
+    os elementos para gerar o código compatível
+*/
